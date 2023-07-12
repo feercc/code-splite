6cb01ce3f47dde4b02a384dcb1ddc4a8a288f835
feer <renyanan@sf-express.com>
2023-07-12 14:50:44 +0800

    设计模式新增

--- test/test.js
+++ test/test.js
@@ -1,33 +1,77 @@
-class Event {
-  constructor() {
-    this._events = {};
+class Chain {
+  constructor(fn) {
+    this.fn = fn;
+    this.successor = null;
   }
-  on(event, cb) {
-    if (!this._events[event]) {
-      this._events[event] = [];
+  setNextSuccessor(successor) {
+    return (this.successor = successor);
+  }
+  passRequest() {
+    let ret = this.fn.apply(this, arguments);
+    if (ret === "nextSuccessor") {
+      return (
+        this.successor &&
+        this.successor.passRequest.apply(this.successor, arguments)
+      );
     }
-    this._events[event].push(cb);
+    return ret;
   }
-  emit(event, ...args) {
-    if (this._events[event]) {
-      this._events[event].forEach((cb) => {
-        cb(...args);
-      });
-    }
-  }
-  off(event, cb) {
-    if (this._events[event]) {
-      this._events[event] = this._events[event].filter((fn) => {
-        return fn !== cb && fn.l !== cb;
-      });
-    }
-  }
-  once(event, cb) {
-    const fn = (...args) => {
-      cb(...args);
-      this.off(event, fn);
-    };
-    fn.l = cb;
-    this.on(event, fn);
+  // 异步方式兼容
+  next() {
+    return (
+      this.successor &&
+      this.successor.passRequest.apply(this.successor, arguments)
+    );
   }
 }
+
+// 例子
+let order500 = function (orderType, pay, stock) {
+  if (orderType === 1 && pay === true) {
+    console.log("500元定金预购，得到100优惠券");
+  } else {
+    return "nextSuccessor";
+  }
+};
+
+let order200 = function (orderType, pay, stock) {
+  if (orderType === 2 && pay === true) {
+    console.log("200元定金预购，得到50优惠券");
+  } else {
+    return "nextSuccessor";
+  }
+};
+
+let orderNormal = function (orderType, pay, stock) {
+  if (stock > 0) {
+    console.log("普通购买，无优惠券");
+  } else {
+    console.log("手机库存不足");
+  }
+};
+
+// 创建职责链
+let chainOrder500 = new Chain(order500);
+let chainOrder200 = new Chain(order200);
+let chainOrderNormal = new Chain(orderNormal);
+
+// 指定职责链中的顺序
+chainOrder500.setNextSuccessor(chainOrder200);
+chainOrder200.setNextSuccessor(chainOrderNormal);
+
+// 最后把请求传递给第一个节点
+chainOrder500.passRequest(1, true, 500); // 500元定金预购，得到100优惠券
+
+// 更方便的方式
+Function.prototype.after = function (fn) {
+  const self = this;
+  return function () {
+    let ret = self.apply(this, arguments);
+    if (ret === "nextSuccessor") {
+      return fn.apply(this, arguments);
+    }
+    return ret;
+  };
+};
+
+let order = order500.after(order200).after(orderNormal);
